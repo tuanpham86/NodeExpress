3c8ed732bdfc82bf2304431640535ba5aa5636e0
tuanpham86 <tuan.phamvan86@gmail.com>
2016-05-06 01:22:54 +0700

    Sign Up

--- NodeExpress/library/app.js
+++ NodeExpress/library/app.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
+
 var app = express();
 
 
@@ -12,11 +17,17 @@ var nav = [{
     Text: 'Author'
     }];
 var bookRouter = require('./src/routes/bookRoutes')(nav);
-
 var adminRouter = require('./src/routes/adminRoutes')(nav);
-
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+
+require('./src/config/passport')(app);
+
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
@@ -24,6 +35,7 @@ app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
